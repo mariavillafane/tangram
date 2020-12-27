e30256a94d82868226c93d15df9cdcea3cad82ea
Maria Villafane <eugeniavil@gmail.com>
2020-12-27 02:33:15 +0000

    added functions image_to_matrix (unflatten array) and to_image_data (flatten array to visualise image

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ function batch(count, arr) {
   return range(arr.length/count).map(x=> arr.slice(x*count, (x+1)*count))
 }
 
+function to_image_matrix(a, b, image_data){
+  const imageRows = batch(a*b, image_data.data) //512*4
+  return imageRows.map(row => batch(b, row)) //4, row 
+}
+
+function to_image_data(imageMatrix){
+  //flatten array js
+  return new Uint8ClampedArray(imageMatrix.flat().flatMap(x => [...x]));
+}
+
 function main() {
   const canvas = document.querySelector(".inputImageCanvas");
   const img = document.querySelector(".inputImage");
@@ -24,33 +34,31 @@ function main() {
   
   const imageData = ctx.getImageData(0, 0, 512, 512);
   const newImageData = 
-    imageData.data.map((x, index) => index< 100000 ? 50 : x );
-
-
+    imageData.data.map((x, index) => index< 300000 ? 50 : x );
+    
+  console.log(newImageData);
 
   //i = range(512) //i = [0,1,..511]
   //const imageRows =  // imageData {length=512*512*4} -> imageRows {length=512*4} 
   //  range(512).map(x=> imageData.data.slice(x*512*4, (x+1)*512*4)) // map(lambda x: ..., i)
   
-  const imageRows = batch(512*4, imageData.data)
+  ///const imageRows = batch(512*4, imageData.data)
 
-
-  //
   //const imageMatrix = // length: 512*4 -> 512
   //  imageRows.map(row => range(512).map(x => row.slice(x*4, (x+1)*4)))  // map(lambda x: ..., i)
   
-  const imageMatrix = imageRows.map(row => batch(4, row))
+  ///const imageMatrix = imageRows.map(row => batch(4, row))
 
+  const imageMatrix = to_image_matrix(512, 4, imageData);
   console.log(imageMatrix);
 
-  const newImage = new ImageData(
-    newImageData, 
-    512, 512);
+  const imageDataAgain = to_image_data(imageMatrix);
+  console.log(imageDataAgain);
 
-
+  const newImage = new ImageData(imageDataAgain, 512, 512);
+  //const newImage = new ImageData(newImageData, 512, 512);
   ctx.putImageData(newImage, 0, 0);
      
-
 }
 
 function App() {
